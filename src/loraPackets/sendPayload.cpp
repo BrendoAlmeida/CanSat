41945d549a42053cd3e4468b41f2b6dbd22a5fc0
Brendo Almeida <84606470+BrendoAlmeida@users.noreply.github.com>
2024-07-23 20:15:13 -0300

    Update Lora

---
+++ src/loraPackets/sendPayload.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brend on 23/07/2024.
+//
+
+#include "sendPayload.h"
---
+++ src/loraPackets/sendPayload.h
@@ -0,0 +1,14 @@
+//
+// Created by brend on 23/07/2024.
+//
+
+#ifndef CANSAT_SENDPAYLOAD_H
+#define CANSAT_SENDPAYLOAD_H
+
+
+class sendPayload {
+
+};
+
+
+#endif //CANSAT_SENDPAYLOAD_H
